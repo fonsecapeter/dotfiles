7fc853972ede579e2e6eba8277f67b3acf9ba778
Peter Fonseca <peter.nfonseca@gmail.com>
2017-09-18 16:56:19 -0700

    Linux quick fix

--- scripts/kde/install_tools.sh
+++ scripts/kde/install_tools.sh
@@ -1,7 +1,7 @@
-sudo apt update && sudo apt upgrade
+sudo apt update && sudo apt full-upgrade
 sudo apt install -y git make tree vim fontforge inkscape gimp python3-pip python3.6 virtualenv xclip sqlite3 screenfetch wget curl fortune cowsay boxes neofetch cmatrix ccat ctags colordiff silversearcher-ag
 
 pip3 install hangups
 sudo pip3 install ipython[all]
 
-sudo gem install peter-notes
+sudo gem install peter-notes pry colorize
