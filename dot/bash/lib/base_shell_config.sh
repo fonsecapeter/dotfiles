50306f5a2d9a1be119d32867b42e695503a3ba8a
Peter Fonseca <pfonseca@23andme.com>
2017-09-25 20:14:23 -0700

    Minimal prompt
    With custom git awesomeness
    Also clean up neofetch a little

--- dot/bash/lib/base_shell_config.sh
+++ dot/bash/lib/base_shell_config.sh
@@ -1,4 +1,38 @@
-export PS1="\[\033[38;5;11m\]\u\[$(tput sgr0)\]\[\033[38;5;15m\]@\[$(tput sgr0)\]\[\033[38;5;1m\]\h\[$(tput sgr0)\]\[\033[38;5;15m\]:\[$(tput sgr0)\]\[\033[38;5;3m\]\W\[$(tput sgr0)\]\[\033[38;5;15m\]\\$ \[$(tput sgr0)\]"
+# export PS1="${bright_orange}\u${bright_white}@${red}\h${bright_white}:${bright_red}\W${bright_white}$ ${reset}"
+my_dash () {
+  if [ ! $? -eq 0 ]; then
+    echo "${bright_red}xx"
+    return 0
+  fi
+  if git rev-parse --git-dir > /dev/null 2>&1; then
+    git_status=$(git status 2>&1)
+    if [[ "${git_status}" =~ "Changes not staged" ]]; then
+      echo -e "${red}**"
+      return 0
+    elif [[ "${git_status}" =~ "Untracked files" ]]; then
+      echo -e "${red}**"
+      return 0
+    elif [[ "${git_status}" =~ "Changes to be committed" ]]; then
+      echo -e "${bright_orange}++"
+      return 0
+    elif [[ "${git_status}" =~ "different commits" ]]; then
+      echo -e "${orange}~~"
+      return 0
+    elif [[ "${git_status}" =~ "branch is ahead" ]]; then
+      echo -e "${orange}~~"
+      return 0
+    elif [[ "${git_status}" =~ "branch is behind" ]]; then
+      echo -e "${orange}~~"
+      return 0
+    elif [[ "${git_status}" =~ "interactive rebase" ]]; then
+      echo -e "${purple}.."
+      return 0
+    fi
+  fi
+  echo -e "${bright_black}--"
+}
+dashes='echo "$(my_dash)"'
+export PS1=" \`${dashes}\` ${bright_green}\W${white}$ ${reset}"
 export LSCOLORS="cxBxhxDxgxhxhxhxhxfxfx"
 
 PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
--- dot/bash/lib/colors.sh
+++ dot/bash/lib/colors.sh
@@ -8,5 +8,17 @@ blue=$(tput setaf 4)
 alias blue="tput setaf 4"
 purple=$(tput setaf 5)
 alias purple="tput setaf 5"
+cyan=$(tput setaf 6)
+white=$(tput setaf 7)
+
+bright_black=$(tput setaf 8)
+bright_red=$(tput setaf 9)
+bright_green=$(tput setaf 10)
+bright_orange=$(tput setaf 11)
+bright_blue=$(tput setaf 12)
+bright_purple=$(tput setaf 13)
+bright_cyan=$(tput setaf 14)
+bright_white=$(tput setaf 15)
+
 reset=$(tput sgr0)
 alias reset="tput sgr0"
