8147806767e134da7edadf6d2f86b6bdde507d0a
Peter Fonseca <pfonseca@23andme.com>
2017-12-07 11:02:44 -0800

    Clean up hyper config
    Also use super simple "$ " zsh prompt

--- dot/hyper/hyper-hi-dpi.js
+++ dot/hyper/hyper-hi-dpi.js
@@ -1,13 +1,102 @@
-// Future versions of Hyper may add additional config options,
-// which will not automatically be merged into this file.
 // See https://hyper.is#cfg for all currently supported options.
+const colors = {
+  black: '#51595e',
+  red: '#e47e71',
+  green: '#90cba6',
+  yellow: '#f6c693',
+  blue: '#8aa9d1',
+  magenta: '#bf94c3',
+  cyan: '#9edad6',
+  white: '#94a1a8',
+  lightBlack: '#7e898e',
+  lightRed: '#f6819e',
+  lightGreen: '#80b7a0',
+  lightYellow: '#f9997e',
+  lightBlue: '#97c3d3',
+  lightMagenta: '#c5a2c0',
+  lightCyan: '#b5ede6',
+  lightWhite: '#c5c8c6'
+}
+const otherColors = {
+  camel: '#f5e6c2',
+  darkBlack: '#393f49'
+}
 
 module.exports = {
   config: {
+    // Choose either "stable" for receiving highly polished,
+    // or "canary" for less polished but more frequent updates
+    updateChannel: 'stable',
+    shell: '/usr/local/bin/zsh',
+    shellArgs: ['--login'],
+    fontSize: 10,
+    fontFamily: '"Peter Mono"',
+
+    // `BEAM` for |, `UNDERLINE` for _, `BLOCK` for █
+    cursorShape: 'BLOCK',
+    cursorBlink: true,
+    cursorColor: '#4ea37e',
+    foregroundColor: otherColors.camel,
+    backgroundColor: '#464d51',
+    borderColor: otherColors.darkBlack,
+
+    padding: '5px',
+    colors: colors,
+
+    // UI
+    css: `
+      .hyper_main {
+        border: none;
+      }
+      footer.footer_footer {
+        font-family: "Peter Mono";
+        -webkit-font-smoothing: subpixel-antialiased;
+        font-size: 10px;
+        background-color: ${otherColors.darkBlack};
+      }
+      footer.footer_footer .footer_group {
+        color: white;
+      }
+      header {
+        font-family: "Peter Mono";
+        -webkit-font-smoothing: subpixel-antialiased;
+        background: ${otherColors.darkBlack};
+        border: none;
+      }
+      header.header_i1rech {
+        top: 0;
+        left: 0;
+        width: 100%;
+      }
+      ul.tabs_list > li.tab_tab: {
+        border: none !important;
+      }
+      .tab_hasActivity {
+        color: ${otherColors.camel};
+      }
+      .tab_active {
+        background: #40484d;
+      }
+    `,
+
+    // Syntax
+    termCSS: `
+      x-screen {
+        -webkit-font-smoothing: subpixel-antialiased !important;
+      }
+    `,
+
+    env: {},
+    bell: false,
+    copyOnSelect: false,
+    showHamburgerMenu: '',
+    showWindowControls: '',
+
+    // Plugins
     hyperStatusLine: {
       footerTransparent: false,
-      dirtyColor: '#f6c693',
-      aheadColor: '#bf94c3'
+      dirtyColor: colors.yellow,
+      aheadColor: colors.magenta
     },
 
     tabIcons: {
@@ -33,180 +122,38 @@ module.exports = {
         ]
       },
       mapColors: {
-        bash: '#c5a2c0',
-        zsh: '#8aa9d1',
-        git: '#e47e71',
-        py: '#8aa9d1',
-        python: '#8aa9d1',
-        ipython: '#8aa9d1',
-        django: '#8aa9d1',
-        rb: '#e47e71',
-        pry: '#e47e71',
-        rake: '#e47e71',
-        irb: '#e47e71',
-        ruby: '#e47e71',
-        rails: '#e47e71',
-        docker: '#97c3d3',
-        node: '#80b7a0',
-        grunt: '#f6c693',
-        http: '#f6c693',
-        curl: '#f6c693',
+        bash: colors.lightMagenta,
+        zsh: colors.blue,
+        git: colors.red,
+        py: colors.blue,
+        python: colors.blue,
+        ipython: colors.blue,
+        django: colors.blue,
+        rb: colors.red,
+        pry: colors.red,
+        rake: colors.red,
+        irb: colors.red,
+        ruby: colors.red,
+        rails: colors.red,
+        docker: colors.lightBlue,
+        npm: colors.red,
+        node: colors.lightGreen,
+        grunt: colors.yellow,
+        http: colors.yellow,
+        curl: colors.yellow,
         make: 'white',
-        'work-up': '#9edad6'
+        'work-up': colors.cyan
       }
-    },
-
-    // Choose either "stable" for receiving highly polished,
-    // or "canary" for less polished but more frequent updates
-    updateChannel: 'stable',
-
-    // default font size in pixels for all tabs
-    fontSize: 10,
-
-    // font family with optional fallbacks
-    fontFamily: '"Peter Mono"',
-
-    // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
-    cursorColor: '#4ea37e',
-
-    // `BEAM` for |, `UNDERLINE` for _, `BLOCK` for █
-    cursorShape: 'BLOCK',
-
-    // set to true for blinking cursor
-    cursorBlink: true,
-
-    // color of the text
-    foregroundColor: '#f5e6c2',
-
-    // terminal background color
-    backgroundColor: '#464d51',
-
-    // border color (window, tabs)
-    borderColor: '#464d51',
-
-    // custom css to embed in the main window
-    css: `
-      .hyper_main {
-        border: none;
-      }
-      footer.footer_footer {
-        font-family: "Peter Mono";
-        -webkit-font-smoothing: subpixel-antialiased;
-        font-size: 10px;
-        background-color: #394044;
-      }
-      footer.footer_footer .footer_group {
-        color: white;
-      }
-      header {
-        font-family: "Peter Mono";
-        -webkit-font-smoothing: subpixel-antialiased;
-        background: #394044;
-        border: none;
-      }
-      header.header_i1rech {
-        top: 0;
-        left: 0;
-        width: 100%;
-      }
-      .tab_hasActivity {
-        color: #f5e6c2;
-      }
-      .tab_active {
-        background: #464d51;
-      }
-    `,
-
-    // custom css to embed in the terminal window
-    termCSS: `
-      x-screen {
-        -webkit-font-smoothing: subpixel-antialiased !important;
-      }
-    `,
-
-    // set to `true` (without backticks) if you're using a Linux setup that doesn't show native menus
-    // default: `false` on Linux, `true` on Windows (ignored on macOS)
-    showHamburgerMenu: '',
-
-    // set to `false` if you want to hide the minimize, maximize and close buttons
-    // additionally, set to `'left'` if you want them on the left, like in Ubuntu
-    // default: `true` on windows and Linux (ignored on macOS)
-    showWindowControls: '',
-
-    // custom padding (css format, i.e.: `top right bottom left`)
-    padding: '5px',
-
-    // the full list. if you're going to provide the full color palette,
-    // including the 6 x 6 color cubes and the grayscale map, just provide
-    // an array here instead of a color map object
-    colors: {
-      black: '#51595e',
-      red: '#e47e71',
-      green: '#90cba6',
-      yellow: '#f6c693',
-      blue: '#8aa9d1',
-      magenta: '#bf94c3',
-      cyan: '#9edad6',
-      white: '#94a1a8',
-      lightBlack: '#7e898e',
-      lightRed: '#f6819e',
-      lightGreen: '#80b7a0',
-      lightYellow: '#f9997e',
-      lightBlue: '#97c3d3',
-      lightMagenta: '#c5a2c0',
-      lightCyan: '#b5ede6',
-      lightWhite: '#c5c8c6'
-    },
-
-    // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
-    // if left empty, your system's login shell will be used by default
-    //
-    // Windows
-    // - Make sure to use a full path if the binary name doesn't work
-    // - Remove `--login` in shellArgs
-    //
-    // Bash on Windows
-    // - Example: `C:\\Windows\\System32\\bash.exe`
-    //
-    // Powershell on Windows
-    // - Example: `C:\\WINDOWS\\System32\\WindowsPowerShell\\v1.0\\powershell.exe`
-    shell: '/usr/local/bin/zsh',
-
-    // for setting shell arguments (i.e. for using interactive shellArgs: ['-i'])
-    // by default ['--login'] will be used
-    shellArgs: ['--login'],
-
-    // for environment variables
-    env: {},
-
-    // set to false for no bell
-    bell: 'false',
-
-    // if true, selected text will automatically be copied to the clipboard
-    copyOnSelect: false
-
-    // if true, on right click selected text will be copied or pasted if no
-    // selection is present (true by default on Windows)
-    // quickEdit: true
-
-    // URL to custom bell
-    // bellSoundURL: 'http://example.com/bell.mp3',
-
-    // for advanced config flags please refer to https://hyper.is/#cfg
+    }
   },
 
-  // a list of plugins to fetch and install from npm
-  // format: [@org/]project[#version]
-  // examples:
-  //   `hyperpower`
-  //   `@company/project`
-  //   `project#1.0.1`
   plugins: [
     'hyper-statusline',
     'hyperterm-cursor',
     'hyperterm-alternatescroll',
     'hyper-nobold',
-    'hyper-tab-icons'
+    'hyper-tab-icons',
+    'hypercwd'
   ],
 
   // in development, you can create a directory under
---
+++ dot/zsh/super-simple.zsh-theme
@@ -0,0 +1,1 @@
+PROMPT='%B%F{white}$ %f%'
