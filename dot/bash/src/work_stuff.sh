9823d3369c9f546ff0f4da619325523ab846d56d
Peter Fonseca <pfonseca@23andme.com>
2018-02-07 09:56:44 -0800

    Stop vim from not clearing screen

--- dot/bash/src/work_stuff.sh
+++ dot/bash/src/work_stuff.sh
@@ -7,7 +7,3 @@ alias docs='cd ~/Documents/code_base/'
 # for printing through django stderr logs
 alias pbprint="echo \"import sys
 sys.stderr.write('\n\n' + repr(object_to_print) + '\n\n')\" | pbcopy"
-
-# docker
-alias dockclean="docker rm -v $(docker ps -a -q -f status=exited)"
-alias dockjango="docker-compose -f dev.yml run django python manage.py"
